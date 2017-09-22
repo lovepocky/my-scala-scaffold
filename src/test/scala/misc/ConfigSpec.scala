53965d2c48b02a64acef0cf8a42dbbceb79b0d7d
Iovepocky <lovepocky@foxmail.com>
2017-09-22 11:36:29 +0800

    update: typesafe config spec

--- src/test/scala/misc/ConfigSpec.scala
+++ src/test/scala/misc/ConfigSpec.scala
@@ -6,15 +6,31 @@ import org.scalatest.FlatSpec
 
 class ConfigSpec extends FlatSpec with LazyLogging {
 
-  "config" should "override in 'include'" in {
+  "standard config" should "override in 'include'" in {
     /**
       * method: ConfigFactory.load() will load standard config
+      *
       * @see [[https://github.com/typesafehub/config#standard-behavior]]
       */
     val standard = ConfigFactory.load()
     assert(
-      "override2" == standard.getString("spec.include.field")
+      standard.getString("spec.include.field") == "override2"
     )
   }
+
+  it should "skip not exist config file" in {
+    val standard = ConfigFactory.load()
+    assert(
+      standard.getString("spec.include.skip") == "skip"
+    )
+  }
+
+  it should "cannot include wildcard template (i cannot find this feature in typesafehub/config)" in {
+    val standard = ConfigFactory.load()
+    assertThrows[com.typesafe.config.ConfigException.Missing](
+      standard.getBoolean("debug.mode")
+    )
+  }
+
 
 }
